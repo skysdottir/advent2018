7f017132f5e43c56b615bdf63c395045360553c9
Danielle Skysdottir <skysdottir@gmail.com>
2018-12-25 13:12:24 -0800

    Finished day 15 part 2, more weird edge cases fixed

--- Day15/fight.py
+++ Day15/fight.py
@@ -3,12 +3,15 @@
 # because all selection happens in page order (left to right, top row first), that just means
 # we'll pick the lowest ID
 
-row_size = 7
-filename = "example4.txt"
+row_size = 32
+filename = "input.txt"
 
 # Initialize the board with closed space so extra volume just doesn't get used
 board=["#"] * row_size**2
-elf_dps = 3
+elf_dps = 20
+max_dps = 30
+
+# We're going to store elves and goblins as loc_id -> (health, name)
 elves = dict()
 goblins = dict()
 elf_count = 0
@@ -39,9 +42,9 @@ def init():
             # because they are blocking objects
             board[id(x, y)] = char
             if char == "E":
-                elves[id(x, y)] = 200
+                elves[id(x, y)] = [200, "E{}{}".format(x, y)]
             elif char == "G":
-                goblins[id(x, y)] = 200
+                goblins[id(x, y)] = [200, "G{}{}".format(x, y)]
 
     elf_count = len(elves)
 
@@ -154,35 +157,35 @@ def select_target(id, is_elf):
     if len(foes) == 0:
         return -1
     elif len(foes) == 1:
-        return foes[0]
+        id = foes[0]
+        return [id, all_foes[id][0], all_foes[id][1]]
     else:
-        
-        pairs = [(i, all_foes[i]) for i in foes]
+        # this will be (location, health, name) triples
+        triples = [[i, all_foes[i][0], all_foes[i][1]] for i in foes]
 
         # magic sort! We want the weakest, and then the lowest ID if there's more than one
-        pairs.sort(key = lambda foe: foe[1]*1000 + foe[0])
-        return pairs[0][0]
+        triples.sort(key = lambda foe: foe[1]*1000 + foe[0])
+        return triples[0]
 
 # 9) attack!
 # Bug discovered in part 2: also need to remove the dead unit from the move order
 # to prevent phantom moves if someone else moves in on top of them
 def kill(id, is_elf):
-    print("Killing unit at {}".format(coord(id)))
     pop = (elves if is_elf else goblins)
+    # print("Killing unit {} at {} from pop {}".format(id, coord(id), pop))
     pop.pop(id)
     board[id] = "."
 
 def attack(attacker_id, target_id, is_elf):
-    print("{} attacking {}!".format(coord(attacker_id), coord(target_id)))
+    # print("{} attacking {}!".format(coord(attacker_id), coord(target_id)))
     # sanity test
     assert board[attacker_id] == ("E" if is_elf else "G")
 
     foes = (goblins if is_elf else elves)
 
     # adjustable attack power for part 2
-    foes[target_id] -= 3
-    if foes[target_id] <= 0:
-        kill(target_id, (not is_elf))
+    foes[target_id][0] -= (elf_dps if is_elf else 3)
+    if foes[target_id][0] <= 0:
         return True
     
     return False
@@ -214,7 +217,7 @@ def test_victory():
     if len(elves) == 0 or len(goblins) == 0:
         elves_win = (len(elves) > 0)
         winners = (elves if elves_win else goblins)
-        remaining_health = sum([v for k,v in winners.items()])
+        remaining_health = sum([v[0] for k,v in winners.items()])
 
         print("And that's a wrap! Last finished round: {} Winner: {}".format(round, "Elves" if elves_win else "Goblins"))
         print("Surviving elves: {} of {}".format(len(elves), elf_count))
@@ -225,37 +228,45 @@ def test_victory():
 
 # and the tick function itself
 def tick():
-    # get move order
-    move_order = [(id, True) for id in elves.keys()]
-    move_order.extend([(id, False) for id in goblins.keys()])
+    dead = []
+    # get move order - triple: (location, name, is_elf)
+    move_order = [(id, val[1], True) for id, val in elves.items()]
+    move_order.extend([(id, val[1], False) for id, val in goblins.items()])
     move_order.sort(key=lambda pair:pair[0])
 
-    print("Round {} move order: {}".format(round, [coord(unit[0]) for unit in move_order]))
+    # print("Round {} move order: {}".format(round, [coord(unit[0]) for unit in move_order]))
 
-    for unit in move_order:
-        print("Ticking unit at {}".format(coord(unit[0])))
+    for unit_triple in move_order:
+        # print("Ticking {} at {}".format(("elf" if unit_triple[2] else "gob"), coord(unit_triple[0])))
         # zeroth, does this unit still exist?
-        char = board[unit[0]]
-        if char not in ["E", "G"]:
-            # print("Dead unit at {}".format(coord(unit[0])))
+        if unit_triple[1] in dead:
+            # print("Unit {} at {} died this round and cannot move".format(unit_triple[1], coord(unit_triple[0])))
             continue
 
+        char = board[unit_triple[0]]
+        if char not in ["E", "G"]:
+            # print("Missing unit at {}".format(coord(unit[0])))
+            continue
+        
         # first, are we done?
         if test_victory():
-            print("Unit {} declares victory!".format(coord(unit[0])))
+            # print("Unit {} declares victory!".format(coord(unit_triple[0])))
             return False
 
         # second, do we need to move?
-        loc = unit_move_phase(unit[0], unit[1])
-        target = select_target(loc, unit[1])
+        loc = unit_move_phase(unit_triple[0], unit_triple[2])
 
-        # tricky bug here - this is in a for loop over move_order, we can't edit move_order
-        # Uuuugh I'm actually going to have to name my units, aren't I.
-        if target != -1:
-            killed = attack(loc, target, unit[1])
-            if killed and (target, not unit[1]) in move_order:
-                # target died, remove from move order if they haven't moved yet
-                move_order.remove((target, not unit[1]))
+        # (location, health, name)
+        target_triple = select_target(loc, unit_triple[2])
+
+        # okay this is getting hideous - now that units have names and locations
+        # what's what is getting hard to juggle and needs structural fixes
+        # Fortunately I only need this to work once
+        if target_triple != -1:
+            if attack(loc, target_triple[0], unit_triple[2]):
+                # target died
+                dead.append(target_triple[2])
+                kill(target_triple[0], not unit_triple[2])
 
     return True
 
@@ -274,12 +285,14 @@ def print_board():
 
 # spun through all the possibilities to find the win condition
 # seems to happen at elf_dps = 25
-print("Running sim at elf_dps " + str(elf_dps))
-init()
-fight = True
-while fight:
-    print_board()
-    fight = tick()
-    round += 1
+while elf_dps < max_dps:
+    print("Running sim at elf_dps " + str(elf_dps))
+    init()
+    fight = True
+    while fight:
+        # print_board()
+        fight = tick()
+        round += 1
 
+    elf_dps += 1
 # print_board()
